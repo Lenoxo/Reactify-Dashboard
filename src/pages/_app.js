345a91c5cc203ef8964d94976e3b530d4321a1aa
Emanuel Padilla <emanuehl159@gmail.com>
2023-06-15 22:18:19 -0500

    Añado layout principal. Avances hasta la parte 8.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css';
-
+import MainLayout from '@layout/MainLayout';
+import '@/styles/globals.css'
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </>
+  );
 }
