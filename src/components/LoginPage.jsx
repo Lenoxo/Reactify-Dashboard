4b85ccf51391216ab137d320ca9ac29a8827a782
Emanuel Padilla <emanuehl159@gmail.com>
2023-06-24 12:47:27 -0500

    Añado el icono del usuario en LoginPage.

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -32,7 +32,7 @@ export default function LoginPage() {
       <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
           <div>
-            <UserCircleIcon className='mx-auto h-16 w-auto text-blue-500'/>
+            <UserCircleIcon className="mx-auto h-16 w-auto text-blue-500" />
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign in to your account</h2>
           </div>
           {loginError && (
