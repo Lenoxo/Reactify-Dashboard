369d089e763da04a991d484f3c4c7249df7db0d3
Emanuel Padilla <emanuehl159@gmail.com>
2023-06-27 22:21:23 -0500

    Corrijo el mensaje de la ruta de Nav. Para que muestre que el usuario está en Home y no un string vacio.

--- src/common/Nav.jsx
+++ src/common/Nav.jsx
@@ -3,11 +3,18 @@ import { useRouter } from 'next/router';
 export default function Nav() {
   const router = useRouter();
   const route = router.pathname.substring(1);
+  function message(route) {
+    if (route === '') {
+      return `You're at Home`;
+    } else {
+      return `You're at ${route}`;
+    }
+  }
 
   return (
     <nav className="bg-white shadow">
       <div className="max-w-7xl mx-auto py-3 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-sm font-bold text-zinc-500 capitalize">You're at {route}</h1>
+        <h1 className="text-sm font-bold text-zinc-500 capitalize">{message(route)}</h1>
       </div>
     </nav>
   );
